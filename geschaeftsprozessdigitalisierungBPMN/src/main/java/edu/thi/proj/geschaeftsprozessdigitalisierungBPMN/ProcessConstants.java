298e5684bcccb03abc57401f83e74c347868cf66
weima <weima@DESKTOP-43AUKRF>
2022-04-06 17:42:13 +0200

    Start of Project

---
+++ geschaeftsprozessdigitalisierungBPMN/src/main/java/edu/thi/proj/geschaeftsprozessdigitalisierungBPMN/ProcessConstants.java
@@ -0,0 +1,7 @@
+package edu.thi.proj.geschaeftsprozessdigitalisierungBPMN;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "geschaeftsprozessdigitalisierungBPMN"; // BPMN Process ID
+
+}
