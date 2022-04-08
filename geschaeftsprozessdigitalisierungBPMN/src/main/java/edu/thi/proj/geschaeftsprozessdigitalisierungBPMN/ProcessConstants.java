c3993aff2f6bcdfb060da130893f4073c3db3dd4
Matthias Weimann <maw3505@thi.de>
2022-04-08 10:05:09 +0200

    reset

--- geschaeftsprozessdigitalisierungBPMN/src/main/java/edu/thi/proj/geschaeftsprozessdigitalisierungBPMN/ProcessConstants.java
+++
@@ -1,7 +0,0 @@
-package edu.thi.proj.geschaeftsprozessdigitalisierungBPMN;
-
-public class ProcessConstants {
-
-  public static final String PROCESS_DEFINITION_KEY = "geschaeftsprozessdigitalisierungBPMN"; // BPMN Process ID
-
-}
