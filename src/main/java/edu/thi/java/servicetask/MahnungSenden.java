53c0d1ab4d047aa4750ba263171b5444fae67ad0
Emreekici44 <eme8731@thi.de>
2022-06-17 13:12:31 +0200

    "Änderung vom 16.06"

---
+++ src/main/java/edu/thi/java/servicetask/MahnungSenden.java
@@ -0,0 +1,26 @@
+package edu.thi.java.servicetask;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
+
+public class MahnungSenden implements JavaDelegate{
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		long rechnungsbetrag = (long) execution.getVariableLocal("Rechnungsbetrag");
+		
+		long mahngebuehr = rechnungsbetrag + 15;
+
+		System.out.println();
+
+	}
+
+
+}
