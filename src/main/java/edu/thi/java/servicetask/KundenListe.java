e1777b4e7adc78240ba26e58b4bd045516f51b94
Matthias Weimann <maw3505@thi.de>
2022-06-22 15:41:55 +0200

    letzter stand

--- src/main/java/edu/thi/java/servicetask/KundenListe.java
+++ src/main/java/edu/thi/java/servicetask/KundenListe.java
@@ -2,7 +2,6 @@ package edu.thi.java.servicetask;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 import org.camunda.bpm.engine.RuntimeService;
@@ -10,21 +9,13 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
 
-public class TerminSpeichern implements JavaDelegate{
+public class KundenListe implements JavaDelegate{
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		
-		//ArrayList<String[]> kundenListe = new ArrayList<String[]>();
-		//kundenListe.add("Gold");
-		
-		
-		HashMap<String, String> kunde = new HashMap<>();
-		kunde.put("Bob", "Gold");
-		kunde.put("Alice", "Silber");
-		kunde.put("Peter", "Bronze");
-		kunde.put("Leon", "Neu");
-		kunde.put("Leonhard", "Gold");
+		//Date termin = new Date();
+		//System.out.println("Termin vom " + termin + " wurde eingetragen");
 		
 		System.out.println("Termin wurde beim Kunden eignetragen");
 	}
